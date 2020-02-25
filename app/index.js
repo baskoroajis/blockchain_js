ec858f8c5b0671b12462bd6a87b78e4389c7cf7f
baskoro aji susetyo <baskoroajisusetyo@gmail.com>
2020-02-25 10:24:30 +0700

    add p2pserver

--- app/index.js
+++ app/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const BlockChain = require('../blockchain');
+const bodyParser = require('body-parser');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
+app.use(bodyParser.json())
+
 
 const app = new express();
 const bc = new BlockChain();
+const p2pServer = new P2pServer(bc);
 
-app.get('/getBlocks',(req,res)=>{
+app.get('/blocks',(req,res)=>{
     res.json(bc.chain);
 })
 
+app.post('/mine', (req,res) => {
+    const newBlock = bc.addBlock(req.body.data);
+    console.log(`new block added to chain ${newBlock}`);
+    res.redirect('/blocks');
+})
+
+
+
 
 app.listen(HTTP_PORT, () => {console.log('server listening on port ', HTTP_PORT)})
+p2pServer.listen();
