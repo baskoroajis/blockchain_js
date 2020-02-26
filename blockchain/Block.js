28863fac3a460d0a8c92011977ee6691822e88d4
baskoro aji susetyo <baskoroajisusetyo@gmail.com>
2020-02-26 09:49:30 +0700

    add dificulty

--- blockchain/Block.js
+++ blockchain/Block.js
@@ -5,12 +5,13 @@ var sha256 = require('crypto-js/sha256');
 const {DIFICULTY} = require('../config');
 
 class Block{
-    constructor(timeStamp, lastHash, hash, data, nonce){
+    constructor(timeStamp, lastHash, hash, data, nonce, dificulty){
         this.timeStamp = timeStamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
         this.nonce = nonce;
+        this.dificulty = dificulty;
     }
 
     toString(){
@@ -20,24 +21,29 @@ class Block{
             Hash : ${this.hash.substring(0,10)}
             Nonce : ${this.nonce}
             data : ${this.data}
+            dificulty : ${this.dificulty}
         `
     }
 
     static mineBlock(lastBlock, data){
         let timeStamp, hash;
         const lastHash = lastBlock.hash;
-        // const hash = Block.hash(timeStamp,lastHash,data);
+        let {dificulty} = lastBlock.dificulty;
         let nonce = 0;
         
         do{
             nonce++;
             timeStamp = Date.now();
+            dificulty 
             hash = Block.hash(timeStamp,lastHash,data,nonce);
         }while(hash.substring(0, DIFICULTY) !== '0'.repeat(DIFICULTY))
 
         return new this(timeStamp, lastHash, hash, data, nonce);
     }
 
+    static adjustDificulty(block, currentTimeStamp){
+        //check is lastblock timestamp + mine_rate is
+    }
     static hash(timeStamp,lastHash,data,nonce){
         return sha256(`${timeStamp}${lastHash}${data}${nonce}`).toString();
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const DIFICULTY = 3;
+//3 seconde or 3000 milisecond
+const MINE_RATE = 3000;
 
-module.exports = {DIFICULTY};
+module.exports = {DIFICULTY, MINE_RATE};
